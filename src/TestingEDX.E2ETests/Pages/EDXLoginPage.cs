f1d496f8530e67496690dd4f3e7654914e05c8e1
Eduardo Vázquez López <eduardo.vazlopez@iberant.es>
2020-10-05 17:36:52 -0400

    Incorrect Loging E2E Test

---
+++ src/TestingEDX.E2ETests/Pages/EDXLoginPage.cs
@@ -0,0 +1,20 @@
+﻿using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TestingEDXDemo.Pages
+{
+    public class EDXLoginPage
+    {
+        private IWebDriver _driver;
+
+        public IWebElement emailInput => _driver.FindElement(By.Name("email"));
+        public IWebElement passwordInput => _driver.FindElement(By.Name("password"));
+        public IWebElement singInButton => _driver.FindElement(By.XPath("//button[@type='submit']"));
+        public IWebElement cantLoginMessage => _driver.FindElement(By.XPath("//h4[@class='message-title']"));
+        public EDXLoginPage(IWebDriver driver) => _driver = driver;
+
+        public void GoToEDXLoginPage() => _driver.Url = "https://courses.edx.org/login";
+    }
+}
---
+++ src/TestingEDX.E2ETests/Steps/EDXIncorrectLoginE2ETest.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using TestingEDXDemo.Pages;
+using Xunit;
+
+namespace TestingEDXDemo.Steps
+{
+    public class EDXIncorrectLoginE2ETest : TestBase
+    {
+        private EDXLoginPage eDXLoginPage;
+        public EDXIncorrectLoginE2ETest() => eDXLoginPage = new EDXLoginPage(driver);
+
+        [Fact]
+        public void Login_WithWrongUserAndPass_CantLogin()
+        {
+            eDXLoginPage.GoToEDXLoginPage();
+
+            eDXLoginPage.emailInput.SendKeys("fakeuser@fakeemail.com");
+            eDXLoginPage.passwordInput.SendKeys("12345");
+            eDXLoginPage.singInButton.Click();
+
+            Assert.True(eDXLoginPage.cantLoginMessage.Displayed);
+        }
+    }
+}
